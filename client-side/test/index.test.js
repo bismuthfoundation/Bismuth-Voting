0d553a90c880627f8c099d411c2b715c0c1ede8b
EggPool <dev@eggpool.net>
2019-09-24 11:43:51 +0200

    More derivation tests, one fails.

--- client-side/test/index.test.js
+++ client-side/test/index.test.js
@@ -10,6 +10,8 @@ const testMnemonic = "letter advice cage absurd amount doctor acoustic avoid let
 const testSeedHex = "95a7ecb56bda5eba808eec2407b418002b824c6c1cb159ec44b8371405629f8429419e98e1b67fc6367368f5d82871a501bbc655a62d31e8391411d7a6e74b86";
 const testPubkeyHex = "0418b9908d43f503ae8ed3128c35edd8e0b9350c01a389bf5d83aece4822722b6223dbfaec15801253d658356836802a43c401fed1415a312f1a09f52d96a52ae4";
 const address_key1HexSeed = "c5d44637eb43b04bfa07b8cf1272e22d95a740126cd9e1ab7363840f118a9ebf6671d081ea1f89f13aad09c1a92dabd62eb1c0e81b701f8116b7401a30a98867";
+const motion_key1aHexSeed = "874b74454b3c073320fcca8aacf0b5cf606550aaa5cac0fa718078ca5ecff6b43ee220e340fbfdd51d9202c2598f2f8930777c4d146c41a2bf915101a4bd72cb";
+const motion_key1bHexSeed = "f5fef68b6e7fbc7d518506a924687e75590ff557b61f4c56f1df1b1ec7b852b80d98113e35eebd3e3df500f4caa071044043c03d47bba613ce5b68c121a62bae";
 
 describe("Seed Tests", () => {
   test("should convert mnemonic to seed", () => {
@@ -30,5 +32,17 @@ describe("Derivation Tests", () => {
     const master_key = new DerivableKey(utils.hexToBytes(testSeedHex));
     const address_key1 = master_key.derive("Bis_test_address1");
     expect(utils.bytesToHex(address_key1.seed)).toBe(address_key1HexSeed);
+  });
+  test("Derive test Motion1a", () => {
+    const master_key = new DerivableKey(utils.hexToBytes(testSeedHex));
+    const address_key1 = master_key.derive("Bis_test_address1");
+    const motion_key1a = address_key1.derive("motion_1_txid_this_would_be_a_b64_encoded_string");
+    expect(utils.bytesToHex(motion_key1a.seed)).toBe(motion_key1aHexSeed);
+  });
+  test("Derive test Motion1b", () => {
+    const master_key = new DerivableKey(utils.hexToBytes(testSeedHex));
+    const address_key1 = master_key.derive("Bis_test_address1");
+    const motion_key1b = address_key1.derive("motion_2_txid_this_would_be_a_b64_encoded_string");
+    expect(utils.bytesToHex(motion_key1b.seed)).toBe(motion_key1bHexSeed);
   });
 });
