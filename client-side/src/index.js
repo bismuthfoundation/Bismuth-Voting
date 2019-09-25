50092eefe7bf101733f1eccea56fa98efec8486c
Aravind Balla <bsaaravind@gmail.com>
2019-09-25 16:15:22 +0530

    change syntax in js

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -3,9 +3,8 @@ const bip39 = require('bip39');
 const crypto = require("crypto");
 const eccrypto = require("eccrypto");
 
-const derivableKey = require("./derivableKey.js");
-const utils = require("./utils.js");
-var DerivableKey = derivableKey.DerivableKey;
+const { DerivableKey, version: derivableKeyVersion } = require("./DerivableKey");
+const utils = require("./utils");
 
 const TEST_SEED_HEX = "95a7ecb56bda5eba808eec2407b418002b824c6c1cb159ec44b8371405629f8429419e98e1b67fc6367368f5d82871a501bbc655a62d31e8391411d7a6e74b86";
 const DEFAULT_PASSWORD = 'BismuthGVP';
@@ -20,28 +19,28 @@ function component() {
   bip39.mnemonicToSeed('letter advice cage absurd amount doctor acoustic avoid letter advice cage above', DEFAULT_PASSWORD).then(bytes => bytes.toString('hex')).then(console.log);
   //bip39.mnemonicToSeed('letter advice cage absurd amount doctor acoustic avoid letter advice cage above', DEFAULT_PASSWORD).then(console.log);
 
-  console.log(derivableKey.version);
+  console.log(derivableKeyVersion);
 
-  var master_key = new DerivableKey(utils.hexToBytes(TEST_SEED_HEX));
+  const master_key = new DerivableKey(utils.hexToBytes(TEST_SEED_HEX));
   console.log("Master " + master_key);
   console.log("Master seed", master_key.seed);
   console.log("Master seed h1", master_key.seed.toString('hex')); // ???
   console.log("Master seed h2", utils.bytesToHex(master_key.seed));
-  var pubkey = master_key.to_pubkey()
+  const pubkey = master_key.to_pubkey()
   console.log("pubkey " + utils.bytesToHex(pubkey));
-  var address_key1 = master_key.derive("Bis_test_address1");
+  const address_key1 = master_key.derive("Bis_test_address1");
   console.log("Seed1 " + utils.bytesToHex(address_key1.seed));
   console.log("AES1 " + utils.bytesToHex(address_key1.to_aes_key()));
-  var motion_key1a = address_key1.derive("motion_1_txid_this_would_be_a_b64_encoded_string");
+  const motion_key1a = address_key1.derive("motion_1_txid_this_would_be_a_b64_encoded_string");
   console.log("Seed1a " + utils.bytesToHex(motion_key1a.seed));
   console.log("AES1a " + utils.bytesToHex(motion_key1a.to_aes_key()));
-  var encrypted = motion_key1a.encrypt_vote('B', true);
+  let encrypted = motion_key1a.encrypt_vote('B', true);
   console.log("encrypted1a B" + utils.bytesToHex(encrypted));
-  console.log("encrypted1a B b64" , Buffer.from(encrypted).toString('base64'));
+  console.log("encrypted1a B b64", Buffer.from(encrypted).toString('base64'));
   // B 7b74501d577bc84c655bfcb3c91fc5f3  - Message e3RQHVd7yExlW/yzyR/F8w==
-  var encrypted = motion_key1a.encrypt_vote('A', true);
+  encrypted = motion_key1a.encrypt_vote('A', true);
   console.log("encrypted1a A" + utils.bytesToHex(encrypted));
-  console.log("encrypted1a A b64" , Buffer.from(encrypted).toString('base64'));
+  console.log("encrypted1a A b64", Buffer.from(encrypted).toString('base64'));
   // A A41fea09e3f2e12886c6a24ff73c203d2 -  Message Qf6gnj8uEohsaiT/c8ID0g==
   return element;
 }
