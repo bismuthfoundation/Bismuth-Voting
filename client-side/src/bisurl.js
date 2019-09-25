b4bb6da7ef9a459a2c26c04cfe6b727c317fbde1
EggPool <dev@eggpool.net>
2019-09-25 12:12:39 +0200

    Correct checksum tests

--- client-side/src/bisurl.js
+++ client-side/src/bisurl.js
@@ -1,12 +1,27 @@
+const createHash = require('create-hash');
+var Ascii85 = require('ascii85').Ascii85;
 
 var bisurl = exports;
 
 bisurl.bisUrl = bisUrl;
 bisurl.checksum = checksum;
 
+
 function checksum(string) {
     //  checksum for bisurl
-    return "TODO";
+    buffer = Buffer.from(string);
+    md5 = createHash('md5').update(buffer).digest();
+    var encoder = new Ascii85({
+        // Use RFC194 table
+        table: [
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '!', '#', '$', '%', '&', '(', ')', '*', '+', '-', ';', '<', '=', '>', '?', '@', '^', '_', '`', '{', '|', '}', '~'
+        ]
+    });
+    encoded = encoder.encode(md5).toString()
+    return encoded;
 }
 
 function bisUrl(transaction) {
